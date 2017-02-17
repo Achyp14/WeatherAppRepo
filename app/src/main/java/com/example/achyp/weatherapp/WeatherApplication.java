72dde20e4a6867783ccb61beeba9644a36638282
achyp <achyp14@gmail.com>
2017-02-17 21:44:42 +0200

    add libs

---
+++ app/src/main/java/com/example/achyp/weatherapp/WeatherApplication.java
@@ -0,0 +1,9 @@
+package com.example.achyp.weatherapp;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Base class for all app.
+ */
+public class WeatherApplication extends MultiDexApplication {
+}
